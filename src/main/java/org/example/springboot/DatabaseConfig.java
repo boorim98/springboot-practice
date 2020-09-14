ddb48d033f2798fa500b77cdba4dfefb06478676
boorim98 <bhuyugirl@gmail.com>
2020-09-14 16:25:48 +0900

    Add: Chapter 3 & 4

---
+++ src/main/java/org/example/springboot/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package org.example.springboot;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+    // h2DB는 기본적으로 휘발성이기에 시스템 재작동시 데이터가 사라짐
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/h2-console/*");
+        return registration;
+    }
+}
+
